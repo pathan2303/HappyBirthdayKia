c849c200db704b95885d792c8750377f9390ed99
shabaz Pathan <shabazshaikh@hotmail.com>
2026-06-08 09:28:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg', '9.jpg', '10.jpg'];
+const images = ['1.jpg', '2.jpg', '3.jpg', '4.jpg', '5.jpg', '6.jpg', '7.jpg', '8.jpg'];
 let index = 0;
 const frame = document.getElementById('photo-frame');
 const music = document.getElementById('bg-music');
